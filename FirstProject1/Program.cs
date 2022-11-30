d4ed24aed38bac7d2519454b2432a486bdf9a2e2
thekadiroglu <116295346+thekadiroglu@users.noreply.github.com>
2022-11-30 17:55:46 +0300

    C# First

---
+++ FirstProject1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim = "emirhan";
+                int tamSayi = 37;
+            double ondalikliSayi = 1.2;
+            DateTime tarih = DateTime.Now;
+            bool cevap0 = true;
+
+            Console.WriteLine(isim);
+            Console.WriteLine(tamSayi);
+            Console.WriteLine(ondalikliSayi);
+            Console.WriteLine(tarih);
+            Console.WriteLine(cevap0);
+           
+
+            //aşağıda kullanıcıya soru soruyoruz
+
+            Console.WriteLine("Hangi şehirde yaşıyorsunuz");
+            string cevap = Console.ReadLine();
+            Console.WriteLine(cevap + " Şehrinin plaka kodu nedir?");
+            //Console.WriteLine("{0} şehrinin plaka kodu nedir?", cevap)
+            string metinselPlakaKodu = Console.ReadLine();
+
+            int sayisalPlakaKodu = Convert.ToInt32(metinselPlakaKodu);
+
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
