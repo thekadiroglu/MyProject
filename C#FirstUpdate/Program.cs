0a0923f95a0963d72a8ff058e6e32579f0c4f8a5
thekadiroglu <116295346+thekadiroglu@users.noreply.github.com>
2022-12-01 22:22:21 +0300

    Update

---
+++ C#FirstUpdate/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string isim = "emirhan";
+            //    int tamSayi = 37;
+            //double ondalikliSayi = 1.2;
+            //DateTime tarih = DateTime.Now;
+            //bool cevap0 = true;
+
+            //Console.WriteLine(isim);
+            //Console.WriteLine(tamSayi);
+            //Console.WriteLine(ondalikliSayi);
+            //Console.WriteLine(tarih);
+            //Console.WriteLine(cevap0);
+
+
+            ////aşağıda kullanıcıya soru soruyoruz
+
+            //Console.WriteLine("Hangi şehirde yaşıyorsunuz");
+            //string cevap = Console.ReadLine();
+            //Console.WriteLine(cevap + " Şehrinin plaka kodu nedir?");
+            ////Console.WriteLine("{0} şehrinin plaka kodu nedir?", cevap)
+            //string metinselPlakaKodu = Console.ReadLine();
+
+            //int sayisalPlakaKodu = Convert.ToInt32(metinselPlakaKodu);
+
+
+            //Console.WriteLine("Lütfen sayı giriniz.");
+            //int sayi1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Lütfen bir sayı daha giriniz");
+            //int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            //int toplam = sayi1 + sayi2;
+            //int fark = sayi1 - sayi2;
+            //int carpim = sayi1 * sayi2;
+            //int bolme = sayi1 / sayi2;
+
+            //Console.WriteLine("Toplam:");
+            //Console.WriteLine("Çıkarma:", fark );
+            //Console.WriteLine("Çarpım:", carpim );
+            //Console.WriteLine("Bölme:", bolme );
+
+
+            //Console.WriteLine("Dikdörtgen çevre ve alan hesaplaması yapan uygulama");
+            //Console.WriteLine("Uzun kenar birimi?");
+            //int uzunKenar;
+            //uzunKenar= Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Kısa kenar birimi?");
+            //int kisaKenar = Convert.ToInt32(Console.ReadLine());
+            //int cevre = 2 * (kisaKenar + uzunKenar);
+            //int alan = kisaKenar * uzunKenar;
+
+            //Console.WriteLine("Dikdörtgenin alanı : {0} , alanı : {1}", cevre, alan);
+
+            //Console.WriteLine("Lütfen bir sayı giriniz:");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //if (sayi > 0)
+            //{
+            //    Console.WriteLine("{0} sayısı pozitiftir.", sayi);
+
+
+            //}
+
+            //else if (sayi < 0)
+
+            //{
+            //    Console.WriteLine("{0} sayısı pozitif değildir.", sayi);
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("0 sayısı girildi.");
+            //  }
+
+            //Console.WriteLine("Vize notunuzu giriniz");
+            //int vize = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Final notunuzu giriniz");
+            //int final = Convert.ToInt32(Console.ReadLine());
+
+            //double basariNotu = vize * 0.4 + final * 0.6;
+
+            //if (basariNotu >= 60)
+            //{
+            //    Console.WriteLine("Geçtiniz");
+            //}
+            //else if  (basariNotu >= 50)
+            //{
+            //    Console.WriteLine("Şartlı geçtiniz");
+            //}
+            //else
+            //{ Console.WriteLine("Kaldınız...");
+            //}
+
+            //SWİTCH CASE YAPISI
+
+            //Console.WriteLine("Lütfen bir harf giriniz");
+            //string use = Console.ReadLine();
+
+            //switch (use)
+            //{
+            //    case "a":
+            //        Console.WriteLine("Ahmet, Ali, Ayşe, Aslı, Aydın");
+            //        break;
+            //        case "b":
+            //            Console.WriteLine("Burak, Bilal, Büşra, Barış, Betül");
+            //        break;
+            //        case "c":
+            //        Console.WriteLine("Celal, Ceyda, Cemre, Cenk, Cem");
+            //        break;
+            //        case "d":
+            //        Console.WriteLine("Demiz, Derya, Damla, Dilek, Dinçer");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Bu karakterle uygun bir isim bulunamadı");
+            //        break;
+            //}
+
+            //Console.WriteLine("Ehliyet belgenizin sınıfını yazınız");
+            //char sinif = Convert.ToChar(Console.ReadLine());
+            //switch (sinif)
+            //{
+            //    case 'a':
+            //        Console.WriteLine("tüm motorlarda geçerli");
+            //        break;
+            //    case 'b':
+            //        Console.WriteLine("Otomobil, Kamyonet");
+            //        break;
+            //    case 'c':
+            //        Console.WriteLine("Kamyon, Çekici");
+            //        break;
+            //    case 'd':
+            //        Console.WriteLine("Minibüs, Otobüs");
+            //        break;
+            //    case 'f':
+            //        Console.WriteLine("Traktör");
+            //        break;
+            //    case 'g':
+            //        Console.WriteLine("İş Makinesi");
+            //        break;
+            //    case 'm':
+            //        Console.WriteLine("Motorlu Bisiklet");
+            //        break;
+
+
+            //    default:
+            //        Console.WriteLine("Doğru düzgün gir şunu");
+            //        break;
+
+
+
+
+
+
+
+
+
+
+
+            //}
+            //int a, b, c;
+            //Console.WriteLine("1. sayıyı giriniz.");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("2. sayıyı giriniz.");
+            //b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("3. Sayıyı giriniz");
+            //c = Convert.ToInt32(Console.ReadLine());
+
+            //if ((a<b && a>c) || (a>b && a<c))
+            //{
+            //    Console.WriteLine("{0} sayısı ortancadır", a);
+            //}
+
+            //else if (b<a && b>c || b<c && b>a)
+            //{
+            //    Console.WriteLine("{0} sayısı ortancadır.", b);
+            //}
+            //else if (c < a && c > b || c > a && c < b)
+            //{
+            //    Console.WriteLine("{0} sayısı ortancadır.", c);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayıların gepsi eşit");
+            //}
+            
+            
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
